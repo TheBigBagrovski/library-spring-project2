5668f5d6fbd891db86a9ba1caf204fb506357fa4
TheBigBagrovski <matvey.bagr77@mail.ru>
2023-02-28 21:59:56 +0300

    Initial commit

---
+++ src/main/java/org/example/util/PersonValidator.java
@@ -0,0 +1,33 @@
+package org.example.util;
+
+import org.example.models.Person;
+import org.example.repositories.PeopleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PersonValidator(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Person person = (Person) target;
+        if(peopleRepository.getByFullName(person.getFullName()).isPresent()) {
+            errors.rejectValue("fullName", "", "Человек с таким ФИО уже существует");
+        }
+    }
+
+}
